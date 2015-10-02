a93beaf2d441d58110bdb36b08301b2b25c5ef29
Stefan <sk@inocr8.co>
2015-10-02 23:11:06 +0100

    Bounded elements to camera

--- Assets/PlayerOneScript.cs
+++ Assets/PlayerOneScript.cs
@@ -17,5 +17,18 @@ public class PlayerOneScript : MonoBehaviour {
 			new_speed.y -= speed * Time.deltaTime;
 		}
 		transform.Translate (new_speed);
+
+		//Bound element on y
+		var viewpointCoord = Camera.main.WorldToViewportPoint(transform.position);
+		if (viewpointCoord.y < 0.0f) {
+			Debug.Log("Bottom edge of view");
+			viewpointCoord.y = 0.0f;
+			transform.position = Camera.main.ViewportToWorldPoint(viewpointCoord);
+		}
+		else if (viewpointCoord.y > 1.0f) {
+			Debug.Log("Top edge of view");
+			viewpointCoord.y = 1.0f;
+			transform.position = Camera.main.ViewportToWorldPoint(viewpointCoord);
+		}
 	}
 }
