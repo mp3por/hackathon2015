ade51f60dd51a83d3e490ce36c94d5a620a6b28d
Ivelin Penchev <ivelinpenchev@gmail.com>
2015-10-02 22:50:29 +0100

    Initial commit

---
+++ Assets/PlayerTwoScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTwoScript : MonoBehaviour {
+	public float speed;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Vector3 new_speed = new Vector3 (0, 0, 0);
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			new_speed.y += speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			new_speed.y -= speed * Time.deltaTime;
+		}
+		transform.Translate (new_speed);
+	}
+}
