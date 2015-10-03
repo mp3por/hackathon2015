44e98af0cca555511794d5e6c4125d85421e5d17
Ivelin Penchev <ivelinpenchev@gmail.com>
2015-10-03 03:16:34 +0100

    Tetris continued

--- Assets/Scripts/TetrisScript.cs
+++ Assets/Scripts/TetrisScript.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class TetrisScript : MonoBehaviour {
 	private const int heightUnits = 70;
 	private const int numberOfTetrisCells = 14;
 	private int columns = heightUnits/numberOfTetrisCells;
-	private const int rows = 14;
+	private const int rows = 18;
 	private int[,] tetrisMatrix;
+	private const int numberOfBlocks = 7;
+	private List<int[,]> blocks = new List<int[,]>();
 
 	// Use this for initialization
 	void Start () {
+		blocks.Add(new int[,] {{1,1},{1,0},{1,0},{0,0}});
+		blocks.Add(new int[,] {{1,0},{1,1},{0,1},{0,0}});
+		blocks.Add(new int[,] {{0,1},{1,1},{1,0},{0,0}});
+		blocks.Add(new int[,] {{1,1},{1,1},{0,0},{0,0}});
+		blocks.Add(new int[,] {{1,1},{0,1},{0,1},{0,0}});
+		blocks.Add(new int[,] {{1,0},{1,1},{1,0},{0,0}});
+		blocks.Add(new int[,] {{1,0},{1,0},{1,0},{1,0}});
+
 		tetrisMatrix = new int[columns,rows];
 		for (int i =0; i<columns; i++) {
 			for(int j=0;j<rows;j++) {
@@ -26,7 +36,15 @@ public class TetrisScript : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D collision) {
 		if (collision.gameObject.CompareTag ("ball")) {
 			// Generate tetris block
-			int [,] block = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+			int[,] block = blocks[Random.Range(0,numberOfBlocks)];
+
+			// Append block to tetrisMatrix
+			int column = (int)(collision.gameObject.transform.position.y + 35) / heightUnits;
+
+			if(column == 0) {
+				column++;
+			}
+
 
 		}
 	}
