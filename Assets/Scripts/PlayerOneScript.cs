2ee90869b259d93d9d9fc7eb69d0aea1dc5c2b72
Velin Kerkov <v.b.k.2007@abv.bg>
2015-10-03 19:12:58 +0100

    uspeh pen4ev

--- Assets/Scripts/PlayerOneScript.cs
+++ Assets/Scripts/PlayerOneScript.cs
@@ -1,35 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerOneScript : MonoBehaviour {
+[System.Serializable]
+public class Boundary
+{
+	public float yMin, yMax;
+}
+
+public class PlayerOneScript : MonoBehaviour
+{
 	public float speed;
-
 	public int score = 0;
-
 	private Rigidbody2D rb;
+	public Boundary boundary;
 
-	void Start () {
+	void Start ()
+	{
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
-	public void incrementScore (int amount) {
+	public void incrementScore (int amount)
+	{
 		Debug.Log ("increment");
 		score += amount;
 	}
 
-	void Update () {
-		float direction = 0.0f;
+	private void move (float amount)
+	{
+		Vector2 movement = new Vector2 (0.0f, amount);
+		GetComponent<Rigidbody2D> ().velocity = movement * speed;
+
+		GetComponent<Rigidbody2D> ().position = new Vector3 (
+			0.0f,
+			Mathf.Clamp (GetComponent<Rigidbody2D> ().position.y, boundary.yMin, boundary.yMax) 
+		);
+	}
+
+	void FixedUpdate ()
+	{
 		if (Input.GetKey (KeyCode.W)) {
-			direction += 1;
+			move (1);
+		} else if (Input.GetKey (KeyCode.S)) {
+			move (-1);
 		}
-		if (Input.GetKey (KeyCode.S)) {
-			direction -= 1;
-		}
-		if (direction != 0) {
-			rb.position += new Vector2 (0.0f, speed * direction * Time.deltaTime);
-		} else {
-			rb.velocity = new Vector2(0f,0f);
-		}
-		rb.rotation = 0f;
 	}
 }
