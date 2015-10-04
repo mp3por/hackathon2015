0d6d8294a5c46b54a827f4cd3281a62aeebd6c5f
Georgi <y0c0vc@gmail.com>
2015-10-04 09:49:36 +0100

    Fixed my ballz

--- Assets/Scripts/BonusBallsController.cs
+++ Assets/Scripts/BonusBallsController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class BonusBallsController : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D collider) {
+
+		collider.GetComponent<BallScript> ().setCooldown(false);
 		GameObject ball = Instantiate(collider, transform.position, transform.rotation) as GameObject;
-		ball.BroadcastMessage ("PushBall");
+//		ball.BroadcastMessage ("PushBall");
 	}
 }
