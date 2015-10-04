0647b7e54e666f27879563e5509f8e4e64a3a8da
Velin Kerkov <v.b.k.2007@abv.bg>
2015-10-04 03:04:08 +0100

    first bonus fix

---
+++ Assets/Scripts/TetrisGroup.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetrisGroup : MonoBehaviour
+{
+
+	void Start() {
+		Debug.Log ("tetrisGroup start");
+		// Default position not valid? Then it's game over
+		if (!isValidGridPos()) {
+			Debug.Log("GAME OVER");
+			Destroy(gameObject);
+		}
+	}
+
+
+	float lastFall = 0;
+
+	bool isValidGridPos ()
+	{        
+		foreach (Transform child in transform) {
+			Vector2 v = TetrisGrid.roundVec2 (child.position);
+			
+			// Not inside Border?
+			if (!TetrisGrid.insideBorder (v))
+				return false;
+			
+			// Block in grid cell (and not part of same group)?
+			if (TetrisGrid.grid [(int)v.x, (int)v.y] != null &&
+			    TetrisGrid.grid [(int)v.x, (int)v.y].parent != transform)
+				return false;
+		}
+		return true;
+	}
+
+	void updateGrid ()
+	{
+		// Remove old children from grid
+		for (int y = 0; y < TetrisGrid.h; ++y)
+			for (int x = 0; x < TetrisGrid.w; ++x)
+				if (TetrisGrid.grid [x, y] != null)
+					if (TetrisGrid.grid [x, y].parent == transform)
+						TetrisGrid.grid [x, y] = null;
+		
+		// Add new children to grid
+		foreach (Transform child in transform) {
+			Vector2 v = TetrisGrid.roundVec2 (child.position);
+			TetrisGrid.grid [(int)v.x, (int)v.y] = child;
+		}        
+	}
+
+	void Update ()
+	{
+		// Move Left
+		if (Input.GetKeyDown (KeyCode.E)) {
+			transform.Rotate (0, 0, -90);
+			
+			// See if valid
+			if (isValidGridPos ())
+				// It's valid. Update grid.
+				updateGrid ();
+			else
+				// It's not valid. revert.
+				transform.Rotate (0, 0, 90);
+		}
+
+		if (Time.time - lastFall >= 1) {
+			// Modify position
+			transform.position += new Vector3 (0, -1, 0);
+			
+			// See if valid
+			if (isValidGridPos ()) {
+				// It's valid. Update grid.
+				updateGrid ();
+			} else {
+				// It's not valid. revert.
+				transform.position += new Vector3 (0, 1, 0);
+				
+				// Clear filled horizontal lines
+				TetrisGrid.deleteFullRows ();
+
+				// Disable script
+				enabled = false;
+			}
+			
+			lastFall = Time.time;
+		}
+	}
+}
---
+++ Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSpawner : MonoBehaviour {
+
+	public GameObject[] groups;
+
+	public void SpawnNext (Vector2 position)
+	{
+		Debug.Log ("SpawnNext:" + groups.Length );
+		Vector2 spawnPosition = new Vector2 (position.y, position.x);
+		int i = Random.Range(0, groups.Length);
+
+		Instantiate (Resources.Load("GroupI V"), transform.position, Quaternion.identity);
+	}
+}
